899274cbd81561b6962da80cd356595456095afb
Daniel Levy <daniellofficial@gmail.com>
2023-09-06 10:57:21 +0300

    Add searchBar header support

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,12 +1,21 @@
-const { Link, NavLink } = ReactRouterDOM
+const { Link, NavLink, useLocation } = ReactRouterDOM
 
 export function AppHeader() {
+    const location = useLocation()
+    const path = location.pathname
+
+    const isMail = path.includes('/mail')
+    const isNote = path.includes('/note')
+
 
     return <header className="app-header">
         <Link to="/">
             <h3>LOGO!</h3>
         </Link>
+        {isMail && <h3>Mail</h3>}
+        {isNote && <h3>Note</h3>}
         <nav>
+
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
             <NavLink to="/mail">Mail</NavLink>
