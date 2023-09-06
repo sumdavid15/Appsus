74c395eef9d73c6c4ea4e5db74f6c5a01958bae5
Daniel Levy <daniellofficial@gmail.com>
2023-09-06 13:24:49 +0300

    Fix bugs and improvments

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -3,8 +3,12 @@
 
 import { MailPreview } from './MailPreview.jsx'
 
-export function MailList() {
-    return ( 
-    <div>Mail list</div>
-    )
+export function MailList({ mails }) {
+  return (
+    <section className="mail-list">
+        <ul className="clean-list mail-list">
+            {mails.map(mail => <MailPreview key={mail.id} mail={mail} />)}
+        </ul>
+    </section>
+  )
 }
