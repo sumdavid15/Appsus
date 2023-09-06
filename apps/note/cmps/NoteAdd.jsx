b3eb6fe8b84d6b4a33a69273924779cd1169dc96
David tade <sumdavid15@gmail.com>
2023-09-06 13:25:25 +0300

    css

--- apps/note/cmps/NoteAdd.jsx
+++ apps/note/cmps/NoteAdd.jsx
@@ -1,11 +1,9 @@
-import { noteService } from "../services/note.service.js"
-
 const { useState } = React
 
 export function NoteAdd({ onCreate }) {
     const [noteTitle, setNoteTitle] = useState('')
     const [noteContent, setNoteContent] = useState('')
-    const [type, setType] = useState('NoteTxt')
+    const [type, setType] = useState('text')
     const [color, setColor] = useState('black')
     const [isPinned, setIsPinned] = useState(false)
 
@@ -20,26 +18,34 @@ export function NoteAdd({ onCreate }) {
         onCreate(note)
     }
 
+    function _placeholderType(type) {
+        if (type === 'text') return 'Type text'
+        if (type === 'img') return 'Enter img url'
+        if (type === 'video') return 'Enter video url'
+    }
+
     return (
-        <section className="note-input-container">
-            <input value={noteTitle} onInput={(e) => setNoteTitle(e.target.value)} />
-            <input value={noteContent} onInput={(e) => setNoteContent(e.target.value)} />
-            <select value={type} onChange={(e) => setType(e.target.value)}>
-                <option value="text">Text</option>
-                <option value="img">Image</option>
-                <option value="video">Video</option>
-            </select>
-            <select value={color} onChange={(e) => setColor(e.target.value)}>
-                <option value="black">Black</option>
-                <option value="red">Red</option>
-                <option value="blue">Blue</option>
-            </select>
-            <input
-                type="checkbox"
-                checked={isPinned}
-                onChange={(e) => setIsPinned(e.target.checked)}
-            />
-            <button onClick={addNote}>Add</button>
+        <section className="note-input-container" style={{ backgroundColor: `${color}` }}>
+            <form onSubmit={addNote}>
+                <input value={noteTitle} onInput={(e) => setNoteTitle(e.target.value)} placeholder='Title' required />
+                <input value={noteContent} onInput={(e) => setNoteContent(e.target.value)} placeholder={_placeholderType(type)} required />
+                <select value={type} onChange={(e) => setType(e.target.value)}>
+                    <option value="text">Text</option>
+                    <option value="img">Image</option>
+                    <option value="video">Video</option>
+                </select>
+                <select value={color} onChange={(e) => setColor(e.target.value)}>
+                    <option value="black">Black</option>
+                    <option value="red">Red</option>
+                    <option value="blue">Blue</option>
+                </select>
+                <input
+                    type="checkbox"
+                    checked={isPinned}
+                    onChange={(e) => setIsPinned(e.target.checked)}
+                />
+                <button>Add</button>
+            </form>
         </section>
     )
 }
--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,11 +1,14 @@
 // note service
 import { utilService } from "../../../services/util.service.js"
 import { asyncStorageService } from "../../../services/async-storage.service.js"
+import { storageService } from "../../../services/storage.service.js"
+
+const NOTE_KEY = 'note-list'
 
 export const noteService = {
     saveNote,
 }
 
 function saveNote(note) {
-
+    asyncStorageService.post(NOTE_KEY, note)
 }
