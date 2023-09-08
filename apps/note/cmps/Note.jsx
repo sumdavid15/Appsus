ad4e5ef56441f88dd7960e378d6694fa42861ded
David tade <sumdavid15@gmail.com>
2023-09-08 03:39:58 +0300

    css

--- apps/note/cmps/Note.jsx
+++ apps/note/cmps/Note.jsx
@@ -1,38 +1,69 @@
 import { ColorPicker } from "./ColorPicker.jsx"
 import { TodoList } from "./Todo.jsx"
 
-const { useState } = React
+const { useState, useEffect, useRef } = React
 
 export function Note({ note, onDeleteNote, onChange }) {
     const [color, setColor] = useState(note.color)
     const [isPinned, setIsPinned] = useState(note.isPinned)
-    const [title, setTitle] = useState(note.noteTitle)
 
-    // const degRef = useRef(utilService.getRandomIntInclusive(-2, 2))
+    const titleRef = useRef(null)
+    const contentRef = useRef(null)
+
+    useEffect(() => {
+        if (titleRef.current) titleRef.current.innerText = note.noteTitle;
+        if (contentRef.current) contentRef.current.innerText = note.noteContent;
+
+        // titleRef.current.innerText = note.noteTitle
+        // contentRef.current.innerText = note.noteContent
+    }, [])
 
     function getVideoId() {
         const url = note.noteContent
         const pattern = /v=([a-zA-Z0-9_-]+)/
         let match = url.match(pattern)
 
-        if (!match) match = ''
+        if (!match) return ''
         return match[1]
     }
 
+
     function renderNoteContent(note) {
+
         switch (note.type) {
             case 'text':
-                return <p contenteditable='true'>{note.noteContent}</p>
+                return (<React.Fragment>
+                    <h1 ref={titleRef} contentEditable='true' onInput={_editText}></h1>
+                    <p ref={contentRef} contentEditable='true' onInput={_editContent}></p>
+                </React.Fragment>)
             case 'img':
-                return <img src={note.noteContent} alt="" />
+                return <React.Fragment>
+                    <img src={note.noteContent} alt="" />
+                    <h1 ref={titleRef} contentEditable='true' onInput={_editText}></h1>
+                </React.Fragment>
             case 'video':
-                return <iframe allow="fullscreen" 
-                    src={`https://www.youtube.com/embed/${getVideoId()}`}>
-                </iframe>
+                return <React.Fragment>
+                    <iframe allow="fullscreen"
+                        src={`https://www.youtube.com/embed/${getVideoId()}`}>
+                    </iframe>
+                    <h1 ref={titleRef} contentEditable='true' onInput={_editText}></h1>
+                </React.Fragment>
             case 'todo':
-                return < TodoList />
+                return <React.Fragment>
+                    <h1 ref={titleRef} contentEditable='true' onInput={_editText}></h1>
+                    <TodoList />
+                </React.Fragment>
             default:
+                return null
         }
+    }
+
+    function _editText(ev) {
+        onChange({ ...note, noteTitle: ev.target.innerText })
+    }
+
+    function _editContent(ev) {
+        onChange({ ...note, noteContent: ev.target.innerText })
     }
 
     function duplicate() {
@@ -42,10 +73,7 @@ export function Note({ note, onDeleteNote, onChange }) {
 
     return (
         <section className='note-card' style={{ backgroundColor: `${color}` }}>
-            <h1 contenteditable='true'>{note.noteTitle}</h1>
-
             {renderNoteContent(note)}
-
             <section className='note-action-section'>
                 <div className="note-action-button-container">
                     <ColorPicker color={color} onChange={(color) => {
@@ -55,7 +83,7 @@ export function Note({ note, onDeleteNote, onChange }) {
                             color,
                         })
                     }} />
-                    <div title='Delete Note' onClick={() => onDeleteNote(note.id)}><i className="fa-solid fa-trash"></i></div>
+                    <div className="delete-btn" title='Delete Note' onClick={() => onDeleteNote(note.id)}><i className="fa-solid fa-trash"></i></div>
                     <input className='pinned' type="checkbox" checked={isPinned} onChange={() => {
                         setIsPinned(!isPinned)
                         return onChange({
@@ -63,7 +91,7 @@ export function Note({ note, onDeleteNote, onChange }) {
                             isPinned: !isPinned
                         })
                     }} />
-                    <div title='Duplicate Note' onClick={duplicate}><i className="fa-solid fa-copy"></i></div>
+                    <div className="duplicate-btn" title='Duplicate Note' onClick={duplicate}><i className="fa-solid fa-copy"></i></div>
                 </div>
             </section>
 
--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -4,7 +4,6 @@ import { Note } from "./Note.jsx";
 export function NoteList({ notes, onDeleteNote, onChange }) {
 
     if (!notes.length) return <div>No notes Saved</div>
-
     return (
         <section className="note-list">
             {notes.map(note => <div key={note.id}><Note note={note} onDeleteNote={onDeleteNote} onChange={onChange} /></div>)}
